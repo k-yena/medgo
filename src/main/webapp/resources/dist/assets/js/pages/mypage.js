1731a9738a5cd34b14dd852796e77bf7a6ffa5a0
user <user@DESKTOP-C5EEQRQ>
2025-09-05 17:59:42 +0900

    Fix: 오류 수정 simple-datatables 수정 vendors 폴더 정리

--- src/main/webapp/resources/dist/assets/js/pages/mypage.js
+++ src/main/webapp/resources/dist/assets/js/pages/mypage.js
@@ -1,50 +1,49 @@
-document.addEventListener("DOMContentLoaded", async () => {
-  // 임시 비밀번호 
+onload = () => {
+  // 임시 비밀번호
   const tempPsw = "1234";
-
-  // --- 마이페이지 입장시 나오는 모달 ---
-  const askPassword = () => {
-    Swal.fire({
-      title: "비밀번호를 입력하세요",
-      input: "password",
-      inputPlaceholder: "임시 비밀번호는 1234입니다",
-      confirmButtonColor: "#14b3ae",
-      cancelButtonColor: "#6c757d",
-      showCancelButton: true,
-      cancelButtonText: "취소",
-      confirmButtonText: "확인",
-      reverseButtons: true,
-      inputAttributes: {
-        maxlength: 10,
-        autocapitalize: "off",
-        autocorrect: "off",
-      },
-      allowOutsideClick: false,
-      allowEscapeKey: false,
-      preConfirm: (value) => {
-        if (!value) {
-          Swal.showValidationMessage("비밀번호를 입력해주세요!");
-          return false;
-        }
-        //입력 비밀번호 value와 비교하는 API요청 보내기
-        if (value !== tempPsw) {
-          Swal.showValidationMessage("비밀번호가 틀렸습니다!");
-          return false;
-        }
-        return true;
-      },
-    }).then((result) => {
-      if (result.isConfirmed) {
-        document.querySelector(".protected-content").style.display = "block";
-      } else if (result.isDismissed) {
-        // 취소 시 메인 페이지로 이동
-        window.location.href = "main.html";
-      }
-    });
-  };
-
   askPassword();
-});
+};
+
+// --- 마이페이지 입장시 나오는 모달 ---
+const askPassword = () => {
+  Swal.fire({
+    title: "비밀번호를 입력하세요",
+    input: "password",
+    inputPlaceholder: "임시 비밀번호는 1234입니다",
+    confirmButtonColor: "#14b3ae",
+    cancelButtonColor: "#6c757d",
+    showCancelButton: true,
+    cancelButtonText: "취소",
+    confirmButtonText: "확인",
+    reverseButtons: true,
+    inputAttributes: {
+      maxlength: 10,
+      autocapitalize: "off",
+      autocorrect: "off",
+    },
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    preConfirm: (value) => {
+      if (!value) {
+        Swal.showValidationMessage("비밀번호를 입력해주세요!");
+        return false;
+      }
+      //입력 비밀번호 value와 비교하는 API요청 보내기
+      if (value !== tempPsw) {
+        Swal.showValidationMessage("비밀번호가 틀렸습니다!");
+        return false;
+      }
+      return true;
+    },
+  }).then((result) => {
+    if (result.isConfirmed) {
+      document.querySelector(".protected-content").style.display = "block";
+    } else if (result.isDismissed) {
+      // 취소 시 메인 페이지로 이동
+      window.location.href = "main.html";
+    }
+  });
+};
 
 // --- 회원정보 수정 후 나오는 토스트 ---
 const infoEditModalEl = document.getElementById("infoEdit");
